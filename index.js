1828f596552213bfcde91b1325205bd48a5aea82
Hayko <Haykhakobyan95@gmail.com>
2016-11-29 23:09:12 +0400

    Timer with setInterval

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
 #!/usr/bin/env node
 
-/**
- * Module dependencies
- */
 const horloge = require('commander');
 
-const startTimer = function (opts) {
-  if (opts.breakDuration) {
-    console.log('Started with --break_duration %s', opts.breakDuration);
+
+const hashvark = function(d){
+  if(d%60===0){ 
+    if(d/60<=5 && d/60>0){
+      console.log(~~(d/60)+"mn left");
+    }
   }
-  if (opts.workDuration) {
-    console.log('Started with --work_duration %s', opts.workDuration);
+  if(d===90){
+      console.log("1mn 30s left");
   }
+  if(d===30 || d===20){
+      console.log(d+"s left");
+  }
+  if(d<=10){
+        console.log(d);
+  }
+  if(d===0) {
+    console.log("Time's up!");
+    clearInterval(a);
+  }
+};
+const funk= function(c){
+  if(c===undefined){
+    c=25;
+  }
+  c=fun(c);
+  const a= setInterval(function(){
+    if(c>=0) {hashvark(c--)}},1000);
+};
+const fun= function(p){
+  return p*60;
 };
 
 horloge
-  .command('start')
-  .description('start the Horloge timer.')
-  .option('-b, --break-duration <duration>', 'Time in minutes for break sessions')
-  .option('-w, --work-duration <duration>', 'Time in minutes for work sessions')
-  .action(startTimer);
+  .command('start <d>')
+  .description('starts the horloge timer with a duration in minutes.')
+  .action(funk);
 
 horloge.parse(process.argv);
